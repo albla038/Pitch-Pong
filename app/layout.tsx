9e6a688fd5a54f91c5c9b399f933e2d00a7613bb
Albert Lännerström <albla038>
2024-09-12 15:09:22 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Pich Pong",
+  description: "Control pong with your instument",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
+
